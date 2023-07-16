f4b0de2e1de3a5448c9b1063e4121259b80859b9
안찬영 <an20171@naver.com>
2023-07-16 13:44:38 +0900

    init repo

---
+++ src/main/java/com/example/mybatis/ContinuousReportApplication.java
@@ -0,0 +1,12 @@
+package com.example.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuousReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContinuousReportApplication.class, args);
+	}
+}
