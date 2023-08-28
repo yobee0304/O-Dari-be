bb5f7e7f998730161fcc5cb590a46f0012480e93
안찬영 <chan20172@gmail.com>
2023-08-28 18:15:16 +0900

    데이터베이스 세팅 파일 보완 (작업중)

--- src/main/java/com/example/mybatis/ContinuousReportApplication.java
+++ src/main/java/com/example/mybatis/ContinuousReportApplication.java
@@ -3,7 +3,7 @@ package com.example.mybatis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.mybatis.model.dao"}) // 찾지 못하는 패키지루트 직접 명시
 public class ContinuousReportApplication {
 
 	public static void main(String[] args) {
